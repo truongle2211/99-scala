864c762b8103ef28ed8b8902b665a0d3fbd74f88
Truong <truongae@gmail.com>
2019-01-24 01:54:34 -0500

    add more logic

--- src/main/scala/logic.scala
+++ src/main/scala/logic.scala
@@ -1,27 +1,58 @@
 package logic {
-  object S99Logic {
-    def not(a: Boolean) = a match {
-      case false => true
+
+  class S99Logic(a: Boolean) {
+    import S99Logic._
+
+    def and(b: Boolean) = (a, b) match {
+      case (true, true) => true
       case _ => false
     }
 
-    def and(a: Boolean, b: Boolean) = (a,b) match {
+    def or(b: Boolean) = (a, b) match {
       case (true, _) => true
       case (_, true) => true
       case _ => false
     }
 
-    def or(a: Boolean, b: Boolean) = (a, b) match {
-      case (false, _) => false
-      case (_, false) => false
-      case _ => true
+    def equ(b: Boolean) = (a and b) or (not(a) and not(b))
+
+    def nand(b: Boolean) = not(a and b)
+
+    def nor(a: Boolean, b: Boolean) = not(a or b)
+
+    def xor(a: Boolean, b: Boolean) = not(a equ  b)
+
+    def impl(a: Boolean, b: Boolean) = a or not(b)
+  }
+
+  object S99Logic {
+
+    implicit def boolean2S99Logic(a: Boolean): S99Logic = new S99Logic(a)
+
+    def not(a: Boolean) = a match {
+      case false => true
+      case _ => false
     }
 
-    def nand(a: Boolean, b: Boolean) = not(and(a,b))
-    def nor(a: Boolean, b: Boolean) = not(or(a,b))
-    def equ(a: Boolean, b: Boolean) = or(and(a,b), and(not(a), not(b)))
-    def xor(a: Boolean, b: Boolean) = not(equ(a,b))
-    def impl(a: Boolean, b: Boolean) = or(a, not(b))
+    def table2(f: (Boolean, Boolean) => Boolean) = {
+      print("A    B    result")
+      for {a <- List(true, false)
+           b <- List(true, false)} {
+        printf("%-5s %-5s %-5s", a, b, f(a, b))
+      }
+    }
 
+    def Gray(n: Int): List[String] = n match {
+      case 1 => List("0", "1")
+      case _ => {
+        val lower = Gray(n-1)
+        lower flatMap (x => List(x+"0", x + "1"))
+      }
+    }
+
+    def huffman[A](ls: List[(A, Int)]): List[A,A] = {
+      throw new NotImplementedError("")
+    }
   }
+
 }
